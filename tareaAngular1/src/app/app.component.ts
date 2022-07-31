3e988b3b059437aa1275ce91e9a8ffc9002fbd28
JaraClaudioEz <claudiojara848@gmail.com>
2022-07-31 19:54:40 -0300

    Agregado de lista y boton

--- tareaAngular1/src/app/app.component.ts
+++ tareaAngular1/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Producto } from './producto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
-  title = 'tareaAngular1';
+
+  mostrar: boolean = false;
+
+  toggleMostrar(){
+    this.mostrar = !this.mostrar;
+  }
+
+  productos: Producto[] = [
+    {nombre: "Zapatillas", costo: "Caras"},
+    {nombre: "Remera", costo: "Baratongui"},
+    {nombre: "Saco", costo: "Un riñon"},
+  ];
+
 }
